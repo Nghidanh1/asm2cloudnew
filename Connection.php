eca2a856fc25bb678d4468ae7bfd6e5612243ec1
Nghidanh1 <122130979+Nghidanh1@users.noreply.github.com>
2023-06-17 16:13:55 +0700

    upload

--- Connection.php
+++ Connection.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '','online_shopping')
+    $conn = mysqli_connect('ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'qavfmdsapbmc9fx9', 'parxpgadrqvk1mpe','zpkbhumomfwds06w')
         or die("Can not connect database" . mysqli_connect_error());
 ?>
