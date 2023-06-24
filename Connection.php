23c3f1648caca6dcadfac4a813257df24148893c
Nghidanh1 <122130979+Nghidanh1@users.noreply.github.com>
2023-06-24 15:27:22 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect('j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'bh81mfgc09owyiq9', 'qxaq9yl4i3h700az','o5k1hrw0zsbqenjr')
+    $conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'flc94sj38s4lq7s4', 'csyu8z6cyv4nhx2n','hsopk1t4t81amk3m')
         or die("Can not connect database" . mysqli_connect_error());
 ?>
